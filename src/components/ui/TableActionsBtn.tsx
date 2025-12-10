d54d164a7f067dce102415526094ae214b3cf985
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2025-12-10 23:10:44 +0200

    fix tableActions

--- src/components/ui/TableActionsBtn.tsx
+++ src/components/ui/TableActionsBtn.tsx
@@ -4,6 +4,7 @@ import { ColorBtn } from "@/types";
 import { Popconfirm } from "@/components/ui/Popconfirm";
 import IconButton from "@mui/material/IconButton";
 import { ReactNode } from "react";
+import { Stack } from "@mui/material";
 
 export type Action<T> = {
   key: string;
@@ -20,24 +21,30 @@ type Props<T> = {
 };
 
 export const TableActionsBtn = <T,>({ entity, actions }: Props<T>) => {
-  return actions.map((action) =>
-    action.hide ? null : action.confirm ? (
-      <Popconfirm
-        key={action.key}
-        title={action.confirm.title}
-        description={action.confirm.description}
-        onConfirm={() => action.onClick(entity)}
-      >
-        <IconButton color={action.color || "primary"}>{action.icon}</IconButton>
-      </Popconfirm>
-    ) : (
-      <IconButton
-        key={action.key}
-        color={action.color || "primary"}
-        onClick={() => action.onClick(entity)}
-      >
-        {action.icon}
-      </IconButton>
-    )
+  return (
+    <Stack direction="row" justifyContent={"end"}>
+      {actions.map((action) =>
+        action.hide ? null : action.confirm ? (
+          <Popconfirm
+            key={action.key}
+            title={action.confirm.title}
+            description={action.confirm.description}
+            onConfirm={() => action.onClick(entity)}
+          >
+            <IconButton color={action.color || "primary"}>
+              {action.icon}
+            </IconButton>
+          </Popconfirm>
+        ) : (
+          <IconButton
+            key={action.key}
+            color={action.color || "primary"}
+            onClick={() => action.onClick(entity)}
+          >
+            {action.icon}
+          </IconButton>
+        )
+      )}
+    </Stack>
   );
 };
