111b4eafa3ce67229f5ba8783d998149a76f8227
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2025-12-11 22:02:28 +0200

    add Footer

---
+++ src/components/ui/Footer.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React from "react";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
+
+export const Footer = () => {
+  return (
+    <Box sx={{ mt: 4, py: 3, textAlign: "center", opacity: 0.7 }}>
+      <Divider sx={{ mb: 2 }} />
+      <Typography variant="body2">
+        © {new Date().getFullYear()} TasksHub. Усі права захищені.
+      </Typography>
+    </Box>
+  );
+};
