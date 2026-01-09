7ac1a7dab76bccfca65291c8714b87d794cb4150
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2026-01-09 17:22:02 +0200

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,7 +11,7 @@ export const Footer = () => {
     <Box sx={{ mt: 4, py: 3, textAlign: "center", opacity: 0.7 }}>
       <Divider sx={{ mb: 2 }} />
       <Typography variant="body2">
-        © {new Date().getFullYear()} Next Admin. Усі права захищені.
+        © {new Date().getFullYear()} Next.js Admin. Усі права захищені.
       </Typography>
     </Box>
   );
